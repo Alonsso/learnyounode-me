fffd6bad0b613d6ae9db3b5f1eb0639e78e7da60
Alonsso <cardenasjairods@gmail.com>
2021-04-15 23:28:37 -0500

    Collect data from a GET request and showing it when finished, it also can be done with pipe and bl module

---
+++ http-collect.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+http.get(process.argv[2], (response) => {
+    let result = '';
+    response.setEncoding('utf8');
+    response.on('data', (data) => {
+        result += data;
+    });
+    response.on('end', () => {
+        console.log(result.length);
+        console.log(result);
+    });
+});
